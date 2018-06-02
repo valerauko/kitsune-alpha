cea908eac7b5419ee04eebfcefa94c239161c6d2
Vale <vale@valerauko.net>
2018-06-02 18:24:27 +0900

    Explode routes

--- src/konkon/handler.clj
+++ src/konkon/handler.clj
@@ -2,45 +2,10 @@
   (:require [reitit.ring :as ring]
             [reitit.swagger :refer [create-swagger-handler]]))
 
-(defn todo [])
-
 (def routes
   (ring/router
-    [ ["/api"
-        {:data {:summary "Mastodon compatible API"}}
-        ["/v1"
-          ["/accounts"
-            ["/:user-id"
-              {:get {:handler todo}}
-              ["/followers"
-                {:get {:handler todo}}]
-              ["/following"
-                {:get {:handler todo}}]
-              ["/statuses"
-                {:get {:handler todo}}]
-              ["/follow"
-                {:post {:handler todo}}]
-              ["/unfollow"
-                {:post {:handler todo}}]
-              ["/block"
-                {:post {:handler todo}}]
-              ["/unblock"
-                {:post {:handler todo}}]
-              ["/mute"
-                {:post {:handler todo}}]
-              ["/unmute"
-                {:post {:handler todo}}]]
-            ["/relationships"
-              {:get {:handler todo}}]
-            ["/search"
-              {:get {:handler todo}}]
-            ["/verify_credentials"
-              {:get {:handler todo}}]
-            ["/update_credentials"
-              {:patch {:handler todo}}]]]
-        ["/v2"]]
-      ["/.well-known"
-        {:data {:summary "WebFinger endpoints"}}]
+    [ mastodon/routes
+      webfinger/routes
       ["/swagger.json"
         {:get {:no-doc true
                :swagger {:info {:title "Konkon API"}}
---
+++ src/konkon/routes/webfinger.clj
@@ -0,0 +1,5 @@
+(ns konkon.routes.webfinger)
+
+(def routes
+  ["/.well-known"
+    {:data {:summary "WebFinger endpoints"}}])
