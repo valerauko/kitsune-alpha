137cea92d5a105979a8a4c26f9e7c15616ca42a9
Unknown <valerauko@gmail.com>
2018-07-01 16:14:53 +0900

    OAuth db mount, also scope splitter utility

---
+++ src/kitsune/db/oauth.clj
@@ -0,0 +1,9 @@
+(ns kitsune.db.oauth
+  (:require [hugsql.core :refer [def-db-fns]]
+            [clojure.string :refer [split join]]))
+
+(def-db-fns "sql/oauth.sql")
+
+(defn array-string
+  [arr]
+  (if (string? arr) (split arr #"\s+") arr))
