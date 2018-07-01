a86576e869da4311768f8da192672100a25944c9
Unknown <valerauko@gmail.com>
2018-07-01 19:43:19 +0900

    Translate Postgres PgArrays to vectors

--- src/kitsune/db/core.clj
+++ src/kitsune/db/core.clj
@@ -1,8 +1,10 @@
 (ns kitsune.db.core
   (:require [hikari-cp.core :refer [make-datasource]]
             [hugsql.core]
+            [clojure.java.jdbc :as jdbc]
             [camel-snake-kebab.extras :refer [transform-keys]]
-            [camel-snake-kebab.core :refer [->kebab-case-keyword]]))
+            [camel-snake-kebab.core :refer [->kebab-case-keyword]])
+  (:import org.postgresql.jdbc.PgArray))
 
 (def options
   {:server-name   "kitsune_db"
@@ -13,6 +15,11 @@
 
 (def conn
   {:datasource (make-datasource options)})
+
+(extend-protocol jdbc/IResultSetReadColumn
+  org.postgresql.jdbc.PgArray
+  (result-set-read-column [value metadata index]
+    (vec (.getArray value))))
 
 ; these are from luminus too
 (defn result-one-snake->kebab
