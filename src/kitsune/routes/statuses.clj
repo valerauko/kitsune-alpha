284767192cfe881c239a536486f0ef4e32be938d
Unknown <valerauko@gmail.com>
2018-07-09 00:16:39 +0900

    Update routes

--- src/kitsune/routes/statuses.clj
+++ src/kitsune/routes/statuses.clj
@@ -1,12 +1,19 @@
 (ns kitsune.routes.statuses
-  (:require [kitsune.wrappers.oauth :as oauth-wrapper]))
+  (:require [kitsune.wrappers.oauth :as oauth]
+            [kitsune.spec.statuses :as spec]
+            [kitsune.handlers.statuses :refer [create-status]]))
 
 (def routes
   ["/api"
    ["/v1"
     ["/statuses"
      {:post {:summary "Post new status"
-             :middleware [oauth-wrapper/bearer-auth]}}
+             :scopes #{"write"}
+             :middleware [oauth/bearer-auth
+                          oauth/enforce-scopes]
+             :parameters {:body ::spec/create-status-request}
+             :responses {200 ::spec/create-status-response}
+             :handler create-status}}
      ["/:id"
       {:get {:summary "Show one status"}
        :delete {:summary "Delete status"}}
