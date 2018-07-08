2cfa7a7716ac1dd335c846891f0a3bd456eb9ca4
Unknown <valerauko@gmail.com>
2018-07-09 00:15:22 +0900

    Placeholder specs

--- src/kitsune/spec/statuses.clj
+++ src/kitsune/spec/statuses.clj
@@ -1,2 +1,20 @@
 (ns kitsune.spec.statuses
   (:require [clojure.spec.alpha :as s]))
+
+(s/def ::text
+  (s/and string?
+         #(< (count %) 500)))
+
+; TODO: use proper check
+(s/def ::in-reply-to
+  string?)
+(s/def ::attachments
+  coll?)
+(s/def ::to
+  coll?)
+(s/def ::cc
+  coll?)
+
+(s/def ::create-status-request
+  (s/keys :req-un [::text]
+          :opt-un [::in-reply-to ::attachments ::to ::cc]))
