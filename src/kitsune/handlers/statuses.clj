76d6563e93d7cff0c2af56fe0d144055e8de7891
Unknown <valerauko@gmail.com>
2018-07-14 00:11:02 +0900

    Basic status creation

--- src/kitsune/handlers/statuses.clj
+++ src/kitsune/handlers/statuses.clj
@@ -1,7 +1,16 @@
 (ns kitsune.handlers.statuses
   (:require [ring.util.http-response :refer :all]
-            [kitsune.handlers.core :refer [defhandler]]))
+            [kitsune.handlers.core :refer [defhandler]]
+            [kitsune.db.statuses :as db]))
+
+(defn process-status-text
+  [text]
+  text)
 
 (defhandler create-status
-  [{{:keys [text in-reply-to attachments to cc]} :body-params :as req}]
-  (ok "todo"))
+  [{{:keys [text in-reply-to attachments to cc]
+     :or {to ["https://www.w3.org/ns/activitystreams#Public"]}} :body-params
+    :as req}]
+  (let [people {:user-id (-> req :auth :user-id) :to to :cc cc}
+        fields {:content (process-status-text text)}]
+    (ok (db/create-status people fields)))) ; this is a dummy
