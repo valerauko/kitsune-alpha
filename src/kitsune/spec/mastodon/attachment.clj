d7d6e69b11b80da30b34bcd461a41923bdfab200
Unknown <vale@valerauko.net>
2018-07-29 01:43:01 +0900

    Spec Attachment

--- src/kitsune/spec/mastodon/attachment.clj
+++ src/kitsune/spec/mastodon/attachment.clj
@@ -6,3 +6,14 @@
 (s/def ::static-url ::common/url)
 (s/def ::emoji
   (s/keys :req-un [::shortcode ::common/url ::static-url]))
+
+(s/def ::type #{"image" "video" "gifv" "unknown"})
+(s/def ::remote-url ::common/url)
+(s/def ::preview-url ::common/url)
+(s/def ::text-url ::common/url)
+(s/def ::meta map?) ; TODO: this gonna need some investigation
+(s/def ::description string?)
+
+(s/def ::attachment
+  (s/keys :req-un [::common/id ::type ::common/url ::preview-url]
+          :opt-un [::remote-url ::text-url ::meta ::description]))
