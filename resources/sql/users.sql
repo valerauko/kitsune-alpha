9694ae48a8edf368f7fd9a26fc7125ab23ff524f
Unknown <vale@valerauko.net>
2018-09-22 22:52:28 +0900

    Follower queries

--- resources/sql/users.sql
+++ resources/sql/users.sql
@@ -28,11 +28,26 @@ delete from users
   where name = :name
   returning true as result
 
+-- :name count-followers :? :1
+select users.id as id, uri, count(follows.id) as followers
+  from users left join follows on users.id = follows.followed
+  where users.name = :name and users.local = true
+  group by users.id
+  limit 1
+
+-- :name followers-of :? :*
+select uri from users
+  right join follows on users.id = follows.follower
+  where follows.followed = :id
+  order by follows.created_at desc
+  offset :offset
+  limit :limit
+
 -- :name find-for-auth :? :1
 select id from users where name = :name and pass_hash = :pass-hash
 
 -- :name find-by-name :? :1
-select * from users where name = :name limit 1
+select * from users where name = :name and local = true limit 1
 
 -- :name find-by-id :? :1
 select * from users where id = :id limit 1
