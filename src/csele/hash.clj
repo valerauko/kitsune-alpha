41d2e924ba1f56f5c7af908766cce6700c58b4e6
Unknown <vale@valerauko.net>
2018-09-25 00:30:57 +0900

    Fix sha3 hash

--- src/csele/hash.clj
+++ src/csele/hash.clj
@@ -3,14 +3,14 @@
   (:import [java.io ByteArrayInputStream]
            [java.util Base64]))
 
-(defn hash-string
+(defn hash-hex
   "SHA3 hash of string"
-  ([input] (hash-string input 512))
-  ([input strength]
-    (let [bytes (.getBytes "foo")
+  ([input] (hash-hex input 512))
+  ([^String input ^Integer strength]
+    (let [bytes (.getBytes input)
           sha3 (org.bouncycastle.jcajce.provider.digest.SHA3$DigestSHA3.
                  strength)]
-      (.update sha3 input)
+      (.update sha3 bytes)
       (org.apache.commons.codec.binary.Hex/encodeHexString
         (.digest sha3)))))
 
