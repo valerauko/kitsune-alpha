b5df68b039dec9fdbca2eb2bc7f0568d149252ad
Unknown <vale@valerauko.net>
2018-09-25 00:30:43 +0900

    Add sha-256 hash for body digest

--- src/csele/hash.clj
+++ src/csele/hash.clj
@@ -1,4 +1,7 @@
-(ns csele.hash)
+(ns csele.hash
+  (:require [byte-streams :as bs])
+  (:import [java.io ByteArrayInputStream]
+           [java.util Base64]))
 
 (defn hash-string
   "SHA3 hash of string"
@@ -10,3 +13,14 @@
       (.update sha3 input)
       (org.apache.commons.codec.binary.Hex/encodeHexString
         (.digest sha3)))))
+
+(defn hash-base64
+  "Base64 encoded SHA-256 hash of input"
+  [^ByteArrayInputStream input]
+  (.reset input)
+  (let [bytes (bs/to-byte-array input)
+        bowel (java.security.MessageDigest/getInstance "SHA-256")
+        encoder (Base64/getEncoder)]
+    (->> bytes
+         (.digest bowel)
+         (.encodeToString encoder))))
