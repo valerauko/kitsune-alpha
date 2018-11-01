f2a902e0b8541a77421b6225cdccdf2ab6afd82e
Unknown <vale@valerauko.net>
2018-11-02 01:32:10 +0900

    Add logging and make refetch-profile safer
    No more 500 on missing uri or request failure

--- src/kitsune/federators/user.clj
+++ src/kitsune/federators/user.clj
@@ -1,13 +1,17 @@
 (ns kitsune.federators.user
   (:require [aleph.http :as http]
-            [byte-streams :as bs]
             [org.bovinegenius [exploding-fish :as uri]]
+            [clojure.tools.logging :as log]
             [jsonista.core :as json]))
 
 (defn refetch-profile
   [user-uri]
-  (let [result (-> @(http/get user-uri
-                              {:headers {:accept "application/activity+json"}})
-                   :body
-                   json/read-value)]
-    result))
+  (log/info (str "Fetching profile of " user-uri))
+  (try
+    (let [result (-> @(http/get (str (uri/port user-uri 3000))
+                                {:headers {:accept "application/activity+json"}})
+                     :body
+                     json/read-value)]
+      result)
+    (catch Throwable ex
+      (log/error ex (str "Error while fetching profile " user-uri)))))
