2218a7d86609f6427bf4a66fd6c97a9f079bc159
Unknown <vale@valerauko.net>
2018-11-02 01:28:34 +0900

    Has to specify for swagger validation

--- src/kitsune/spec/mastodon/application.clj
+++ src/kitsune/spec/mastodon/application.clj
@@ -5,6 +5,9 @@
 (s/def ::name string?)
 (s/def ::website ::common/url)
 
+(s/def ::object-vector
+  (s/coll-of map?))
+
 (s/def ::application
   (s/keys :req-un [::name]
           :opt-un [::website]))
