df1a3e9fda395e46151b4e6a711de049501f69b4
taurauko <balint.erdos@studist.jp>
2018-11-23 16:21:16 +0900

    Benchmark and log inbox requests

--- src/kitsune/federators/inbox.clj
+++ src/kitsune/federators/inbox.clj
@@ -5,7 +5,18 @@
             [kitsune.db.statuses :as activity]
             [kitsune.federators.user :as fed]))
 
+(defmacro benchmark-inbox [type id body]
+  `(let [start# (System/nanoTime)
+         result# ~body]
+     (log/info (format "Processed %s %s in %.3fms"
+                       ~type ~id
+                       (/ (- (System/nanoTime) start#) 1000000.0)))
+     result#))
+
 (defn signed?
+  "Checks HTTP header signature of the activity's request.
+   Refetches the user's public key if it doesn't work at first try.
+   Depending on the user's instance that request can be pretty slow."
   [{{sig-header :signature} :headers
     {{actor :actor} :body} :parameters
     :as request}]
@@ -17,9 +28,10 @@
       (headers/verify request refetched-key))))
 
 (defn record
-  [{{{:strs [id type object actor]
+  [{{{:keys [id type object actor]
       :as activity} :body} :parameters
     :as request}]
-  (if (and (signed? request)
-           (not (activity/known-activity? id)))
-    (clojure.pprint/pprint activity)))
+  (benchmark-inbox type id
+    (if (and (signed? request)
+             (not (activity/known-activity? id)))
+      (clojure.pprint/pprint activity))))
