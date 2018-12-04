baed8dc53540f62f30ad8da3065e355cf9214d1f
Unknown <vale@valerauko.net>
2018-12-05 01:05:38 +0900

    Fix federation bugs
    * Mastodon can't handle single-digit days in date
    * Pleroma demands the digest header

--- src/kitsune/federators/outbox.clj
+++ src/kitsune/federators/outbox.clj
@@ -15,9 +15,15 @@
 (defn header-time
   []
   (let [gmt (ZoneId/of "GMT")
-        formatter (DateTimeFormatter/RFC_1123_DATE_TIME)
+        ; for some reason RFC_1123_DATE_TIME isn't good enough. not sure where
+        ; the problem is but mastodon fails to validate the date if the day
+        ; number doesn't have a leading zero (and it doesn't in the rfc format)
+        formatter (DateTimeFormatter/ofPattern "EEE, dd MMM uuu HH:mm:ss zzz")
         timestamp (ZonedDateTime/now gmt)]
     (.format formatter timestamp)))
+
+(def content-type
+   "application/ld+json; profile=\"https://www.w3.org/ns/activitystreams\"")
 
 (def ap-context
   {(keyword "@context") ["https://www.w3.org/ns/activitystreams",
@@ -27,9 +33,10 @@
   [inbox actor activity]
   (let [start (System/nanoTime)
         body (-> (merge ap-context activity) json/write-value-as-bytes)
-        headers {"content-type" "application/activity+json"
+        headers {"host" (uri/host inbox)
                  "date" (header-time)
-                 "host" (uri/host inbox)}
+                 "digest" (str "SHA-256=" (csele.hash/hash-base64 body))
+                 "content-type" content-type}
         key-map {:key-id (str (:uri actor) "#main-key")
                  :pem (:private-key actor)}
         signature (sign-request {:uri (uri/path inbox)
@@ -48,4 +55,7 @@
                           (-> response :request-time float)
                           (/ (- (System/nanoTime) start) 1000000.0))))
       (catch Exception e
-        (log/warn e (str "Failed to send " (:type activity) " to " inbox))))))
+        (log/warn e (str "Failed to send " (:type activity) " to " inbox " -- "
+                         ; if the error's not from aleph it won't have .data
+                         (try (-> e .data :body byte-streams/to-string)
+                           (catch Exception e nil))))))))
