a9356eb45fb8eb76905756a798167a0678e6a2be
Unknown <vale@valerauko.net>
2018-12-08 23:01:46 +0900

    Dynamically read version from lein

--- src/kitsune/instance.clj
+++ src/kitsune/instance.clj
@@ -1,13 +1,15 @@
 (ns kitsune.instance
   (:require [cprop.core :refer [load-config]]
-            [cprop.source :refer [from-env]]
+            [cprop.source :refer [from-env from-system-props]]
             [org.bovinegenius [exploding-fish :refer [uri path]]]
             [mount.core :refer [defstate]]))
 
-(def version "0.1.0")
-
 (defstate config
-  :start (load-config :merge [(from-env)]))
+  :start (load-config :merge [(from-env)
+                              (from-system-props)]))
+
+(def version
+  (:kitsune-version config))
 
 (defn url
   ([]
