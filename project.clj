fceadc60fa5f6f61b38caec241b11e544f3a27a6
Vale <valerauko@gmail.com>
2021-03-30 00:09:45 +0900

    Upgrade dependencies

--- project.clj
+++ project.clj
@@ -3,44 +3,44 @@
   :url "https://kitsune.social"
   :license {:name "GNU AFFERO GENERAL PUBLIC LICENSE"
             :url "https://www.gnu.org/licenses/agpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/tools.namespace "0.2.11"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [org.clojure/tools.namespace "1.1.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [ch.qos.logback/logback-classic "1.2.3"]
                  [ch.qos.logback.contrib/logback-json-classic "0.1.5"]
                  [ch.qos.logback.contrib/logback-jackson "0.1.5"]
-                 [cprop "0.1.13"]
+                 [cprop "0.1.17"]
                  [mount "0.1.16"]
                  [aleph "0.4.6"]
                  [byte-streams "0.2.4"]
-                 [camel-snake-kebab "0.4.0"]
-                 [metosin/reitit "0.3.1"]
-                 [metosin/spec-tools "0.9.1"]
-                 [metosin/muuntaja "0.6.4"]
-                 [metosin/ring-http-response "0.9.1"]
-                 [metosin/jsonista "0.2.2"]
+                 [camel-snake-kebab "0.4.2"]
+                 [metosin/reitit "0.5.12"]
+                 [metosin/spec-tools "0.10.5"]
+                 [metosin/muuntaja "0.6.8"]
+                 [metosin/ring-http-response "0.9.2"]
+                 [metosin/jsonista "0.3.1"]
                  [ring/ring-defaults "0.3.2"]
                  [ring-cors "0.1.13"]
                  [org.bovinegenius/exploding-fish "0.3.6"]
-                 [org.postgresql/postgresql "42.2.5"]
-                 [com.layerware/hugsql "0.4.9"]
-                 [hikari-cp "2.7.1"]
-                 [ragtime "0.8.0"]
-                 [social.kitsune/vuk "0.2.0"]
-                 [social.kitsune/csele "0.4.3"]
-                 [social.kitsune/karak "0.1.1"]
-                 [cljstache "2.0.1"]]
+                 [org.postgresql/postgresql "42.2.19"]
+                 [com.layerware/hugsql "0.5.1"]
+                 [hikari-cp "2.13.0"]
+                 [ragtime "0.8.1"]
+                 [social.kitsune/vuk "0.2.1"]
+                 [social.kitsune/csele "0.5.0"]
+                 [social.kitsune/karak "0.1.2"]
+                 [cljstache "2.0.6"]]
   :main ^:skip-aot kitsune.core
   :target-path "target/%s"
   :jvm-opts ["-Xmx600m" "-server"]
   :profiles {:uberjar {:aot :all
                        :source-paths ["config/prod"]
                        :uberjar-name "kitsune.jar"}
-             :dev {:dependencies [[org.clojure/test.check "0.9.0"]]
+             :dev {:dependencies [[org.clojure/test.check "1.1.0"]]
                    :source-paths ["config/dev"]
-                   :plugins [[jonase/eastwood "0.3.4"
+                   :plugins [[jonase/eastwood "0.3.14"
                               :exclusions [org.clojure/clojure]]
-                             [lein-kibit "0.1.6"]
-                             [lein-cloverage "1.0.13"
+                             [lein-kibit "0.1.8"]
+                             [lein-cloverage "1.2.2"
                               :exclusions [org.clojure/clojure]]
-                             [lein-ancient "0.6.15"]]}})
+                             [lein-ancient "0.7.0"]]}})
